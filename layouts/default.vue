0169179d65bc166da5daf1c67e7e73e70e52f235
Lev <47007280+Lev-Laptev@users.noreply.github.com>
2022-05-30 13:11:50 +0300

    feat: add default layouts

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="w-full pb-12 antialiased">
+    <div class="mx-auto max-w-8xl">
+      <Nuxt />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'LayoutDefault'
+}
+</script>
