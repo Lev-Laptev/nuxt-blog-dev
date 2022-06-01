18926810fed0f595a76a872ae9491dcb1a5353e9
Lev <47007280+Lev-Laptev@users.noreply.github.com>
2022-06-01 10:47:49 +0300

    feat: include components

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,23 @@
 <template>
   <section class="w-full pb-12 antialiased">
     <div class="mx-auto max-w-8xl">
+      <AppHeader />
       <Nuxt />
+      <AppFooter />
     </div>
   </section>
 </template>
 
 <script>
+import AppFooter from '@/components/AppFooter.vue'
+import AppHeader from '@/components/AppHeader.vue'
+
 export default {
-  name: 'LayoutDefault'
+  name: 'LayoutDefault',
+
+  components: {
+    AppFooter,
+    AppHeader
+  }
 }
 </script>
