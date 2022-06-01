530cb20f57e69a282058a7cad201da6f4a12878f
Lev <47007280+Lev-Laptev@users.noreply.github.com>
2022-06-01 14:50:29 +0300

    feat:  add page index

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,21 @@
 <template>
-  <div />
+  <div class="flex flex-column align-start align-self-start justify-start">
+    <h1>Welcome to your dashboard</h1>
+    <p>You are now logged in {{ $fire.auth.currentUser.email }}</p>
+    <v-btn @click.prevent="logout">
+      Logout
+    </v-btn>
+  </div>
 </template>
 
 <script>
+
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  methods: {
+    logout () {
+      this.$fire.auth.signOut()
+    }
+  }
 }
 </script>
