a4baae25159fa0c53b399b1251492b0d0325f1d9
Lev <47007280+Lev-Laptev@users.noreply.github.com>
2022-06-07 16:21:19 +0300

    feat: update AppHeader

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,3 +1,15 @@
 <template>
-  <div></div>
+  <app-navbar />
 </template>
+
+<script>
+import AppNavbar from '@/components/AppNavbar.vue'
+
+export default {
+  name: 'LayoutDefault',
+
+  components: {
+    AppNavbar
+  }
+}
+</script>
