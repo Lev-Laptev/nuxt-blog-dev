3e0a63de6c3bf9d4a82c59b5800efbefe9febbf7
Lev <47007280+Lev-Laptev@users.noreply.github.com>
2022-06-07 17:39:56 +0300

    feat: update page projects

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,9 +1,27 @@
 <template>
-  <div />
+  <div>
+    <app-projects />
+
+    {{ projects }}
+  </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import AppProjects from '@/components/AppProjects.vue'
+
 export default {
-  name: 'projectsPage'
+  name: 'ProjectsPage',
+  components: {
+    AppProjects
+  },
+  async fetch ({ store }) {
+    try {
+      await store.dispatch('projects/getProjects')
+    } catch (e) {}
+  },
+  computed: {
+    ...mapState('projects', ['projects'])
+  }
 }
 </script>
