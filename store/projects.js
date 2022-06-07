8852d07f8ff4ee67b765a71160f3b1b3de4a69a6
Lev <47007280+Lev-Laptev@users.noreply.github.com>
2022-06-07 17:38:10 +0300

    feat: add store projects

---
+++ store/projects.js
@@ -0,0 +1,19 @@
+export const state = () => ({
+  projects: []
+})
+
+export const getters = {}
+
+export const actions = {
+  async getProjects ({ commit }) {
+    const projects = await this.$axios.$get('https://api.github.com/users/lev-laptev/repos')
+
+    commit('ADD_PROJECTS', projects)
+  }
+}
+
+export const mutations = {
+  ADD_PROJECTS (state, projects) {
+    state.projects = projects
+  }
+}
