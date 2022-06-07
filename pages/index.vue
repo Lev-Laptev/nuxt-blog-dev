cdd1dc0b6576f81d315d114d07f351e6ae317dfc
Lev <47007280+Lev-Laptev@users.noreply.github.com>
2022-06-07 16:26:59 +0300

    feat: update page index

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,9 @@
 <template>
-  <div class="flex flex-column align-start align-self-start justify-start">
-    <h1>Welcome to your dashboard</h1>
-    <p>You are now logged in {{ $fire.auth.currentUser.email }}</p>
-    <v-btn @click.prevent="logout">
-      Logout
-    </v-btn>
-  </div>
+  <div />
 </template>
 
 <script>
-
 export default {
-  name: 'IndexPage',
-  methods: {
-    logout () {
-      this.$fire.auth.signOut()
-    }
-  }
+  name: 'defaultPage'
 }
 </script>
