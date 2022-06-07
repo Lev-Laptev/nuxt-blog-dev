b6547e24faed80cdaaee7b341a73e48c96416da7
Lev <47007280+Lev-Laptev@users.noreply.github.com>
2022-06-07 18:16:32 +0300

    feat: add components AppProjectsHeader

---
+++ components/projects/AppProjectsHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="hero is-small is-info">
+    <div class="hero-body has-text-centered">
+      <p class="title">
+        Projects
+      </p>
+      <p class="subtitle">
+        Some of the projects I've build or been a part of.
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AppProjectsHeader'
+}
+</script>
