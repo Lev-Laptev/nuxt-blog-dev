ba3e3201fd9fde50b61f963d4ed89fd5f1bd0857
Lev <47007280+Lev-Laptev@users.noreply.github.com>
2022-06-08 13:28:00 +0300

    feat: update components AppProjects

--- components/projects/AppProjects.vue
+++ components/projects/AppProjects.vue
@@ -1,7 +1,67 @@
 <template>
   <div>
     <app-projects-header />
-    {{ projects }}
+
+    <div class="columns">
+      <div class="column is-12">
+        <div
+          v-for="item in projects"
+          :key="item.id"
+          class="card"
+        >
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">
+                  {{ item.name }}
+                </p>
+                <p class="subtitle is-6">
+                  {{ item.description }}
+                </p>
+              </div>
+            </div>
+
+            <div class="content">
+              <a
+                class="button is-info is-link is-outlined is-small"
+                target="_blank"
+                :href="item.html_url"
+              >
+                Перейти
+              </a>
+
+              <span class="tag is-success">
+                {{ new Date(item.updated_at).toLocaleDateString() }}
+              </span>
+
+              <b-field grouped group-multiline>
+                <div class="control">
+                  <b-taglist attached>
+                    <b-tag type="is-dark">
+                      language
+                    </b-tag>
+                    <b-tag type="is-info">
+                      {{ item.language ? item.language : 'not found' }}
+                    </b-tag>
+                  </b-taglist>
+                </div>
+
+                <div class="control">
+                  <b-taglist attached>
+                    <b-tag type="is-dark">
+                      size
+                    </b-tag>
+                    <b-tag type="is-success">
+                      {{ item.size }} KB
+                    </b-tag>
+                  </b-taglist>
+                </div>
+              </b-field>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
