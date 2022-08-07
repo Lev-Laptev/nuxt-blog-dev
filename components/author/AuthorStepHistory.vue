0f26955c3a1fa2444149df90e962e83019f91712
Lev <47007280+Lev-Laptev@users.noreply.github.com>
2022-08-07 15:36:29 +0300

    feat: add component AuthorStepHistory

---
+++ components/author/AuthorStepHistory.vue
@@ -0,0 +1,62 @@
+<template>
+  <b-steps
+    size="large"
+    type="is-info"
+  >
+    <b-step-item label="Моризо Диджитал" icon="account-key">
+      <div class="content">
+        <h3>Frontend Developer</h3>
+
+        <ul>
+          <li>Верстка, оптимизация сайтов по макетам</li>
+          <li>Поддержка текущих проектов компании</li>
+          <li>Разработка frontend части сайтов с использованием Nuxt Vue</li>
+        </ul>
+
+        <nuxt-link to="https://morizo.ru/">
+          View
+        </nuxt-link>
+      </div>
+    </b-step-item>
+
+    <b-step-item label="Helicon" icon="account">
+      <div class="content">
+        <h3>Frontend Developer</h3>
+
+        <ul>
+          <li>Верстка сайтов по макетам</li>
+          <li>Поддержка текущих проектов компании</li>
+          <li>Оптимизация кода</li>
+          <li>Внесение, изменение контентаа</li>
+        </ul>
+
+        <nuxt-link to="https://helicon-prom.ru/">
+          View
+        </nuxt-link>
+      </div>
+    </b-step-item>
+
+    <b-step-item label="TradeSoft" icon="account-plus">
+      <div class="content">
+        <h3>Junior Frontend Developer</h3>
+
+        <ul>
+          <li>Разработка frontend части интернет-магазинов автозапчастей на базе собственного продукта компании</li>
+          <li>Введение текущих проектов</li>
+          <li>Разработка и поддержка новых базовых адаптивных дизайнов</li>
+          <li>Оптимизация и модернизация существующего подход</li>
+        </ul>
+
+        <nuxt-link to="https://www.tradesoft.ru/">
+          View
+        </nuxt-link>
+      </div>
+    </b-step-item>
+  </b-steps>
+</template>
+
+<script>
+export default {
+  name: 'AuthorStepHistory'
+}
+</script>
