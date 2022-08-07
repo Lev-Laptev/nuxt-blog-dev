4161f311c5cb8ebeb92ab786956e47ec2bde8c53
Lev <47007280+Lev-Laptev@users.noreply.github.com>
2022-08-07 15:36:59 +0300

    feat: include components

--- components/author/index.js
+++ components/author/index.js
@@ -1,1 +1,3 @@
 export { default as AuthorCard } from './AuthorCard.vue'
+export { default as AuthorContent } from './AuthorContent.vue'
+export { default as AuthorStepHistory } from './AuthorStepHistory.vue'
