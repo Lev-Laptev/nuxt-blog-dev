7931ed734ee12d5a2dc04a0e6df3518caa059107
Lev <47007280+Lev-Laptev@users.noreply.github.com>
2022-08-07 15:35:38 +0300

    refactor: refactor AuthorCard

--- components/author/AuthorCard.vue
+++ components/author/AuthorCard.vue
@@ -1,64 +1,62 @@
 <template>
-  <div class="colums">
-    <div class="column is-4">
-      <div class="card">
-        <div class="card-image">
-          <b-image
-            :src="require('@/assets/image/avatar.png')"
-            ratio="4by4"
-            alt="avatar"
-          >
-            <template #placeholder>
-              <b-skeleton
-                class="skeleton-placeholder"
-                height="100%"
-              />
-            </template>
-          </b-image>
+  <div class="card">
+    <div class="card-image">
+      <b-image
+        :src="require('@/assets/image/avatar.png')"
+        ratio="4by4"
+        alt="avatar"
+      >
+        <template #placeholder>
+          <b-skeleton
+            class="skeleton-placeholder"
+            height="100%"
+          />
+        </template>
+      </b-image>
+    </div>
+    <div class="card-content">
+      <div class="media">
+        <div class="media-left">
+          <figure class="image is-48x48">
+            <img :src="require('@/assets/image/avatar.png')" alt="Placeholder image">
+          </figure>
         </div>
-        <div class="card-content">
-          <div class="media">
-            <div class="media-left">
-              <figure class="image is-48x48">
-                <img :src="require('@/assets/image/avatar.png')" alt="Placeholder image">
-              </figure>
-            </div>
-            <div class="media-content">
-              <p class="title is-4">
-                Lev Laptev
-              </p>
-              <p class="subtitle is-6">
-                Frontend Develop
-              </p>
-            </div>
-          </div>
-
-          <div class="content">
-            <Mail />
-            <Glob />
-            <Github />
-          </div>
+        <div class="media-content">
+          <p class="title is-4">
+            Lev Laptev
+          </p>
+          <p class="subtitle is-6">
+            Frontend Develop
+          </p>
         </div>
       </div>
+
+      <div class="content">
+        <a href="mailto:common.levlaptev@gmail.ru">
+          <Mail />
+        </a>
+        <a href="https://github.com/Lev-Laptev" target="_blank">
+          <Github />
+        </a>
+      </div>
     </div>
-    <div class="column is-8" />
   </div>
 </template>
 
 <script>
-import Glob from '@/assets/icons/glob.svg?inline'
 import Github from '@/assets/icons/github_new.svg?inline'
 import Mail from '@/assets/icons/mail.svg?inline'
 
 export default {
-  components: { Mail, Glob, Github }
+  name: 'AuthorCard',
+  components: { Mail, Github }
 }
 </script>
 
 <style scoped>
-.b-skeleton {
+  .b-skeleton {
     height: 100%;
     position: absolute;
     top: 0;
-}
+  }
 </style>
